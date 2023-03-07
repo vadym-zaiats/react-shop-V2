e4f2aa6fcbc5c5eccc01f345e5735a7b4cfaba42
Vadim Zayats <vadyk.za@gmail.com>
2023-03-07 15:14:01 +0530

    project inited

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 // import styles from "./Button.module.scss";
-class Button extends Component {
-  render() {
-    const { text, onClick } = this.props;
-    return <button onClick={onClick}>{text}</button>;
-  }
-}
+
+const Button = ({ text, onClick }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
 Button.propTypes = {
   text: PropTypes.string,
   onClick: PropTypes.func,
