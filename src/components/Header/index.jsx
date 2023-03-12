16fe516c42b2ba7fb1724d971b822a626cdf8656
Vadim Zayats <vadyk.za@gmail.com>
2023-03-12 16:12:56 +0530

    ready

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import basketIco from "../../img/basket.svg";
 import favourite from "../../img/favourite.svg";
+import home from "../../img/home.svg";
 import styles from "./Button.module.scss";
 import { Link, Outlet } from "react-router-dom";
 const Header = ({ basketLength, favouritesLength }) => {
   return (
     <>
       <header className={styles.header}>
-        <Link to="/">Home</Link>
+        <Link to="/">
+          <img src={home} alt="favourite" />
+        </Link>
         <div className={styles.header_favourite}>
           <div>{favouritesLength}</div>
           <Link to="/favourite">
